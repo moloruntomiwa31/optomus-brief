b4b1c918a43041613f6022e5672c622d963e7475
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2023-10-04 15:36:06 -0700

    Added event listener for mobile device

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="w-full flex items-center justify-center space-x-8 mt-3">
-        <input type="text" placeholder="Search books via title..." v-model.trim="searchDetail" @keyup.enter="getBookTitle" @keyup.13="getBookTitle"
-            class="placeholder:text-white dark:placeholder:text-[#3e3939] shadow-orange-500 shadow-sm p-3 outline-none rounded-lg w-2/5 bg-[#3e3939] text-white dark:bg-white dark:text-gray-600">
+        <div class="grid gap-4">
+            <input type="text" placeholder="Search books via title..." v-model.trim="searchDetail"
+                @keyup.enter="getBookTitle"
+                class="placeholder:text-white dark:placeholder:text-[#3e3939] shadow-orange-500 shadow-sm p-3 outline-none rounded-lg bg-[#3e3939] text-white dark:bg-white dark:text-gray-600">
+            <button @click="getBookTitle" class="lg:hidden dark:bg-orange-500 p-2 rounded-lg text-white w-fit">Search</button>
+        </div>
         <h2 class="dark:text-white text-[#3e3939] text-xl capitalize">Hi, {{ reader.user?.email?.split("@")[0] ||
             "bookworm" }}<font-awesome-icon icon="fa-regular fa-face-smile" bounce size="lg"></font-awesome-icon></h2>
     </div>
